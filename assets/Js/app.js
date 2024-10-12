4f69167478d35c95a6c2bd89fb00ca3eeb52d4c8
NITISH KUMAR <siddharthk391@gmail.com>
2024-10-12 15:00:31 +0530

    bug fixed

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -12,6 +12,9 @@ function changetodarktheme() {
 
 // loader script
 window.addEventListener('load', function() {
-    document.getElementById('preloader').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+    setTimeout(() => {
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+        }, 500);
 });
+
